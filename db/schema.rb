256d987ba3b6f4586cbd37564116e2fce9802008
Wiktor Budzich <wikitorb@gmail.com>
2024-12-18 20:56:49 +0100

    136. Creating Permission Model and Permission_Role Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_18_182112) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_18_195126) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,6 +38,21 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_18_182112) do
     t.index ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
   end
 
+  create_table "permission_roles", force: :cascade do |t|
+    t.integer "role_id", null: false
+    t.integer "permission_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["permission_id"], name: "index_permission_roles_on_permission_id"
+    t.index ["role_id"], name: "index_permission_roles_on_role_id"
+  end
+
+  create_table "permissions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -45,4 +60,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_18_182112) do
   end
 
   add_foreign_key "admins", "roles"
+  add_foreign_key "permission_roles", "permissions"
+  add_foreign_key "permission_roles", "roles"
 end
