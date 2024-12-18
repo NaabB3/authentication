9c5d5ecea809634ae1ee369a775ad91256b4f103
Wiktor Budzich <wikitorb@gmail.com>
2024-12-18 19:23:54 +0100

    Kolejne poprawki

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_18_181757) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_18_182112) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,6 +27,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_18_181757) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_admins_on_role_id"
