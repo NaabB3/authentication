087a6e48dd3da0fc18d2581b149b440497331d03
Wiktor Budzich <wikitorb@gmail.com>
2024-12-18 20:09:48 +0100

    129. Creating New Roles By SuperUser

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,8 +1,27 @@
 class RolesController < ApplicationController
   before_action :authenticate_admin!
 
-  
+
   def index
     @roles = Role.all
+  end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.new(role_params)
+    if @role.save
+      flash[:notice] = 'Role created successfully'
+      redirect_to roles_path
+    else
+      flash[:notice] = @role.errors.full_messages.join(",")
+      redirect_to new_role_path
+    end
+  end
+
+  def role_params
+    params.require(:role).permit(:name)
   end
 end
