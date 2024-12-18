e827bea287d9e5179c837e6a8c71ba4c2ee66fc1
Wiktor Budzich <wikitorb@gmail.com>
2024-12-18 19:19:21 +0100

    Poprawki

---
+++ db/migrate/20241218181757_add_lockable_to_admins.rb
@@ -0,0 +1,8 @@
+class AddLockableToAdmins < ActiveRecord::Migration[7.2]
+  def change
+    add_column :admins, :failed_attempts, :integer, default: 0, null: false
+    add_column :admins, :unlock_token, :string
+    add_column :admins, :locked_at, :datetime
+    add_index :admins, :unlock_token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_18_171139) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_18_181757) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,9 +24,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_18_171139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role_id", null: false
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_admins_on_role_id"
+    t.index ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
   end
 
   create_table "roles", force: :cascade do |t|
