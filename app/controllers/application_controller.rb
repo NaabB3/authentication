393becd99fafc89ad20a03c6f2710c3ddd9edde9
Wiktor Budzich <wikitorb@gmail.com>
2024-12-21 08:29:29 +0100

    138. Configuring Pundit Gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  include Pundit::Authorization
+
+  def admin_not_authorized
+    flash[:alert] = "You are not authorized Person to perform this action."
+    redirect_to root_path
+  end
+
+  def pundit_user
+    current_admin
+  end
 end
