175b8eb97932aee9f5d76c9ba9dcdf30212120ae
Wiktor Budzich <wikitorb@gmail.com>
2024-12-21 09:33:28 +0100

    142. Configuring Sessions Controller For Consecutive Failed Login Attempts

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -9,9 +9,29 @@ class Admins::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = Admin.find_for_database_authentication(email: params[:admin][:email])
+
+    if resource && resource.valid_password?(params[:admin][:password])
+      sign_in(resource_name, resource)
+      set_flash_message!(:notice, :signed_in)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      if resource && !resource.super_admin?
+        remain_attempts = (Admin.maximum_attempts - resource.failed_attempts) - 1
+        if(remain_attempts <= 0)
+          super
+        else
+          resource.update failed_attempts: (resource.failed_attempts + 1)
+          flash[:alert] = "Invalid Email or Password. You have #{remain_attempts} more login attempts. After that your account will be locked!."
+        end
+      else
+        flash[:alert] = "Invalid Email or Password"
+      end
+      redirect_to new_admin_session_path
+    end
+  end
+  
 
   # DELETE /resource/sign_out
   # def destroy
